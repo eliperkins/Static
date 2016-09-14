f11916a1b4e368cd6055e06237865e6d932c4c98
Eli Perkins <eli.perkins@venmo.com>
2016-09-14 17:16:22 -0400

    Add dynamic row example

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -69,7 +69,8 @@ class ViewController: TableViewController {
                         self.showAlert(title: "Deleted.")
                     })
                 ])
-            ])
+            ]),
+            dynamicSection(dynamicRowCount, string: dynamicRowString)
         ]
     }
 
@@ -80,5 +81,48 @@ class ViewController: TableViewController {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
         alert.addAction(UIAlertAction(title: button, style: .Cancel, handler: nil))
         presentViewController(alert, animated: true, completion: nil)
+    }
+
+    private var dynamicRowCount = 0 {
+        didSet {
+            var sections = Array(dataSource.sections.dropLast())
+            sections.append(dynamicSection(dynamicRowCount, string: dynamicRowString))
+            dataSource.sections = sections
+        }
+    }
+
+    private var dynamicRowString = "None" {
+        didSet {
+            var sections = Array(dataSource.sections.dropLast())
+            sections.append(dynamicSection(dynamicRowCount, string: dynamicRowString))
+            dataSource.sections = sections
+        }
+    }
+
+    private func dynamicSection(count: Int, string: String) -> Section {
+        return Section(rows: [
+            Row(text: "Tap this row", detailText: String(self.dynamicRowCount), selection: { [unowned self] in
+                self.dynamicRowCount += 1
+            }, cellClass: Value1Cell.self),
+            Row(text: "Tap this row", detailText: self.dynamicRowString, selection: { [unowned self] in
+                let viewController = TableViewController(style: .Grouped)
+                viewController.dataSource.sections = [
+                    Section(rows: [
+                        self.dynamicRow("Dope"),
+                        self.dynamicRow("Sick")
+                    ])
+                ]
+                self.showViewController(viewController, sender: nil)
+            }, cellClass: Value1Cell.self)
+        ])
+    }
+
+    private func dynamicRow(string: String) -> Row {
+        let selectedRow = dynamicRowString == string
+        let accessory: Row.Accessory = selectedRow ? .Checkmark : .None
+        return Row(text: string, selection: { [unowned self] in
+            self.dynamicRowString = string
+            self.navigationController?.popViewControllerAnimated(true)
+        }, accessory: accessory, cellClass: Value1Cell.self)
     }
 }
